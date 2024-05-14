90bdab49c86ab3dfab41af42b7e64947649c144d
R0F7 <wwwrafikhan075@gmail.com>
2024-05-14 22:05:13 +0600

    add blog responsive

--- src/pages/AddBlog.jsx
+++ src/pages/AddBlog.jsx
@@ -2,6 +2,8 @@
 import useAuth from "../hooks/useAuth";
 import toast from "react-hot-toast";
 import useAxiosSecure from "../hooks/useAxiosSecure";
+import { FaNfcSymbol } from "react-icons/fa6";
+// import formBG from '../../public/image/form bg.jpg';
 
 const AddBlog = () => {
     const { user } = useAuth();
@@ -20,65 +22,79 @@ const AddBlog = () => {
         const user_name = user?.displayName;
         const user_image = user?.photoURL;
         const user_email = user?.email;
-        const blogInfo = {image, title, category, short_description, long_description, createdAt, user_name , user_image, user_email};
+        const blogInfo = { image, title, category, short_description, long_description, createdAt, user_name, user_image, user_email };
 
-        axiosSecure.post(`/blog`,blogInfo)
-        .then(() => {
-            // console.log(res.data);
-            toast.success('Submit Info Successful')
-            form.reset()
-        })
-        .catch(error => {
-            // console.log(error);
-            toast.error(error.message)
-        })
-            
+        axiosSecure.post(`/blog`, blogInfo)
+            .then(() => {
+                // console.log(res.data);
+                toast.success('Submit Info Successful')
+                form.reset()
+            })
+            .catch(error => {
+                // console.log(error);
+                toast.error(error.message)
+            })
+
     }
-      
+
     return (
-        <div className="flex w-full min-h-[calc(100vh-350px)] items-center my-12">
-            <section className="w-[70%] p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
-                <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Add Blog Info</h2>
+        <div className="flex w-full min-h-[calc(100vh-350px)] items-center my-12 ">
+            <section className="w-full lg:w-[70%]  mx-auto md:bg-gray-100 rounded-md md:shadow-md dark:bg-gray-800 "
+                // style={{ backgroundImage: `url('${formBG}')`, backgroundSize: 'cover', backgroundPosition: 'top', }}
+            >
+                <div className="bg- py-3 md:p-6 ">
+                    <h2 className="text-xl font-bold text-[#14456A] capitalize dark:text-white">Add Blog Info</h2>
 
-                <form onSubmit={handleSubmit}>
-                    <div className="grid grid-cols-1 gap-x-6 gap-y-4 mt-4 sm:grid-cols-6">
-                        <div className="col-span-3">
-                            <label className="text-gray-700 dark:text-gray-200" htmlFor="username">Image url</label>
-                            <input id="username" type="text" name="photo" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" placeholder="Image url" required/>
+                    <form onSubmit={handleSubmit}>
+                        <div className="grid grid-cols-1 gap-x-6 gap-y-2.5 md:gap-y-4 mt-4 sm:grid-cols-6">
+                            <div className="md:col-span-3">
+                                <label className="text-[#14456A] dark:text-gray-200 font-medium flex items-center gap-1" htmlFor="username"><span className='text-[8px]'>
+                                    <FaNfcSymbol />
+                                </span>Image url</label>
+                                <input id="username" type="text" name="photo" className="block w-full px-4 py-2 mt-2 text-gray-700 shadow bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" placeholder="Image url" required />
+                            </div>
+
+                            <div className="md:col-span-3">
+                                <label className="text-[#14456A] dark:text-gray-200 font-medium flex items-center gap-1" htmlFor="emailAddress"><span className='text-[8px]'>
+                                    <FaNfcSymbol />
+                                </span>Title</label>
+                                <input id="emailAddress" type="text" name="title" className="block w-full px-4 py-2 mt-2 text-gray-700 shadow bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" placeholder="Blog Title" required />
+                            </div>
+
+                            <div className="md:col-span-2">
+                                <label className="text-[#14456A] dark:text-gray-200 font-medium flex items-center gap-1" htmlFor="category"><span className='text-[8px]'>
+                                    <FaNfcSymbol />
+                                </span>Category</label>
+                                <select name="category" id="category" defaultValue='' className="block w-full px-4 py-2 mt-2 text-gray-700  bg-white shadow border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required>
+                                    <option value="" disabled>Select Category</option>
+                                    <option value="Food">Food</option>
+                                    <option value="Travel">Travel</option>
+                                    <option value="Art">Art</option>
+                                    <option value="Technology">Technology</option>
+                                    <option value="Self-Improvement">Self-Improvement</option>
+                                    <option value="Health">Health</option>
+                                </select>
+                            </div>
+
+                            <div className="md:col-span-4">
+                                <label className="text-[#14456A] dark:text-gray-200 font-medium flex items-center gap-1" htmlFor="passwordConfirmation"><span className='text-[8px]'>
+                                    <FaNfcSymbol />
+                                </span>Short Description</label>
+                                <input id="passwordConfirmation" type="text" name="short_description" className="block w-full px-4 py-2 mt-2 text-gray-700 shadow bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required />
+                            </div>
+                        </div>
+                        <div className="col-span-6 mt-2.5 md:mt-6">
+                            <label className="text-[#14456A] dark:text-gray-200 font-medium flex items-center gap-1" htmlFor="passwordConfirmation"><span className='text-[8px]'>
+                                <FaNfcSymbol />
+                            </span>Long Description</label>
+                            <textarea name="long_description" id="" cols="10" rows="7" className="block w-full px-4 py-2 mt-2 text-gray-700 shadow bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></textarea>
                         </div>
 
-                        <div className="col-span-3">
-                            <label className="text-gray-700 dark:text-gray-200" htmlFor="emailAddress">Title</label>
-                            <input id="emailAddress" type="text" name="title" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" placeholder="Blog Title" required/>
+                        <div className="flex justify-end mt-6">
+                            <button className="w-full md:w-1/2 px-8 h-10 leading-5 text-white transition-colors duration-300 transform bg-[#14456A] rounded-md font-medium hover:bg-white hover:text-[#14456A] hover:border hover:font-bold focus:outline-none focus:bg-[#14456A] focus:text-white ">submit</button>
                         </div>
-
-                        <div className="col-span-2">
-                            <label className="text-gray-700 dark:text-gray-200" htmlFor="password">category</label>
-                            <select name="category" id="" defaultValue='' className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required>
-                                <option value="" disabled>Select Category</option>
-                                <option value="Food">Food</option>
-                                <option value="Travel">Travel</option>
-                                <option value="Art">Art</option>
-                                <option value="Technology">Technology</option>
-                                <option value="Self-Improvement">Self-Improvement</option>
-                                <option value="Health">Health</option>
-                            </select>
-                        </div>
-
-                        <div className="col-span-4">
-                            <label className="text-gray-700 dark:text-gray-200" htmlFor="passwordConfirmation">short description</label>
-                            <input id="passwordConfirmation" type="text" name="short_description" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required/>
-                        </div>
-                    </div>
-                    <div className="col-span-6 mt-6">
-                        <label className="text-gray-700 dark:text-gray-200" htmlFor="passwordConfirmation">long description</label>
-                        <textarea name="long_description" id="" cols="10" rows="7" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></textarea>
-                    </div>
-
-                    <div className="flex justify-end mt-6">
-                        <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">submit</button>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </section>
         </div>
     );
