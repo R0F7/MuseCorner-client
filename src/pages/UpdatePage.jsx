4ff8c7d90b84166e9ec5ba30efbac9235e6b340e
R0F7 <wwwrafikhan075@gmail.com>
2024-05-15 10:09:51 +0600

    update page responsive

--- src/pages/UpdatePage.jsx
+++ src/pages/UpdatePage.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import useAxiosSecure from "../hooks/useAxiosSecure";
 import useAuth from "../hooks/useAuth";
 import { useQuery } from "@tanstack/react-query";
+import updateBG from '../../public/image/formbg2.jpg';
 
 const UpdatePage = () => {
     const axiosSecure = useAxiosSecure()
@@ -89,25 +90,27 @@ const UpdatePage = () => {
     }
 
     return (
-        <div className="flex w-full min-h-[calc(100vh-350px)] items-center my-12 bg-gray-50 py-16 rounded-xl">
-            <section className="w-[70%] p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
-                <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Update Blog Info</h2>
+        <div className="flex w-full min-h-[calc(100vh-350px)] items-center my-6 lg:my-12 bg-gray-50 md:p-10 lg:py-16 rounded-xl"
+        style={{ backgroundImage: `url('${updateBG}')`, backgroundSize: 'cover', backgroundPosition: 'top', }}
+        >
+            <section className="w-full lg:w-[70%] p-4 md:p-6 mx-auto glass-container rounded-md shadow-md dark:bg-gray-800 ">
+                <h2 className="text-lg font-semibold text-white capitalize dark:text-white">Update Blog Info</h2>
 
                 <form onSubmit={handleSubmit}>
-                    <div className="grid grid-cols-1 gap-x-6 gap-y-4 mt-4 sm:grid-cols-6">
-                        <div className="col-span-3">
-                            <label className="text-gray-700 dark:text-gray-200" htmlFor="username">Image url</label>
-                            <input id="username" type="text" name="photo" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" defaultValue={image} required />
+                    <div className="grid grid-cols-1 gap-x-6 gap-y-3 md:gap-y-4 mt-4 sm:grid-cols-6">
+                        <div className="md:col-span-3">
+                            <label className="text-white text-sm font-medium dark:text-gray-200" htmlFor="username">Image url</label>
+                            <input id="username" type="text" name="photo" className="block w-full px-4 py-2 mt-2 text-white bg-transparent border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" defaultValue={image} required />
                         </div>
 
-                        <div className="col-span-3">
-                            <label className="text-gray-700 dark:text-gray-200" htmlFor="emailAddress">Title</label>
-                            <input id="emailAddress" type="text" name="title" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" defaultValue={title} required />
+                        <div className="md:col-span-3">
+                            <label className="text-white text-sm font-medium dark:text-gray-200" htmlFor="emailAddress">Title</label>
+                            <input id="emailAddress" type="text" name="title" className="block w-full px-4 py-2 mt-2 text-white bg-transparent border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" defaultValue={title} required />
                         </div>
 
-                        <div className="col-span-2">
-                            <label className="text-gray-700 dark:text-gray-200" htmlFor="password">category</label>
-                            <select name="category" id="" defaultValue='' className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required>
+                        <div className="md:col-span-2">
+                            <label className="text-white text-sm font-medium dark:text-gray-200" htmlFor="password">Category</label>
+                            <select name="category" id="" defaultValue='' className="block w-full px-4 py-2 mt-2 text-white bg-[#14456A] border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required>
                                 <option value="" disabled>Select Category</option>
                                 <option value="Food">Food</option>
                                 <option value="Travel">Travel</option>
@@ -118,18 +121,18 @@ const UpdatePage = () => {
                             </select>
                         </div>
 
-                        <div className="col-span-4">
-                            <label className="text-gray-700 dark:text-gray-200" htmlFor="passwordConfirmation">short description</label>
-                            <input id="passwordConfirmation" type="text" name="short_description" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" defaultValue={short_description} required />
+                        <div className="md:col-span-4">
+                            <label className="text-white text-sm font-medium dark:text-gray-200" htmlFor="passwordConfirmation">Short Description</label>
+                            <input id="passwordConfirmation" type="text" name="short_description" className="block w-full px-4 py-2 mt-2 text-white bg-transparent border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" defaultValue={short_description} required />
                         </div>
                     </div>
-                    <div className="col-span-6 mt-6">
-                        <label className="text-gray-700 dark:text-gray-200" htmlFor="passwordConfirmation">long description</label>
-                        <textarea name="long_description" id="" cols="10" rows="7" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" defaultValue={long_description} required></textarea>
+                    <div className="md:col-span-6 mt-6">
+                        <label className="text-white text-sm font-medium dark:text-gray-200" htmlFor="passwordConfirmation">Long Description</label>
+                        <textarea name="long_description" id="" cols="10" rows="7" className="block w-full px-4 py-2 mt-2 text-white bg-transparent border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" defaultValue={long_description} required></textarea>
                     </div>
 
                     <div className="flex justify-end mt-6">
-                        <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Update</button>
+                        <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform border-2 border-[#14456A] bg-[#14456A] rounded-md hover:bg-transparent hover:border-2 focus:outline-none focus:bg-gray-600">Update</button>
                     </div>
                 </form>
             </section>
